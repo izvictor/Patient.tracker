cc9f094ed82a48e9251947d5d5e3cbd8e0e6e19f
Victor Izverschi <victor.izverschi@outlook.com>
2017-11-19 22:17:01 +0100

    Adding patient context and patient model

---
+++ Patient.Tracker.DataAccess/Context/PatientTrackerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PatientModel = Patient.Tracker.Model.Patient;
+namespace Patient.Tracker.DataAccess.Context
+{
+    public class PatientTrackerDbContext : IdentityDbContext<PatientModel, IdentityRole<int>, int>
+    {
+        public PatientTrackerDbContext(DbContextOptions<PatientTrackerDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Patient.Tracker;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Patient.Tracker.Model/Patient.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Patient.Tracker.Model
+{
+    public class Patient : IdentityUser<int>
+    {
+        public string SocialSecurityNumber { get; set; }
+
+    }
+}
