271c28d68f2dbb4f4eef7f1575ade7c9a0d8fdcd
Victor Izverschi <victor.izverschi@outlook.com>
2017-11-23 12:28:49 +0100

    Code cleanup

--- Patient.Tracker.API/Startup.cs
+++ Patient.Tracker.API/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Patient.Tracker.API
 {
@@ -30,9 +24,7 @@ namespace Patient.Tracker.API
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseMvc();
         }
