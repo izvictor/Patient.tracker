e6cc9ca2c81e8f9df7598aefe1d8804989508b79
Victor Izverschi <victor.izverschi@outlook.com>
2017-12-03 11:21:47 +0100

    Adding patient controller

--- Patient.Tracker.API/Program.cs
+++ Patient.Tracker.API/Program.cs
@@ -5,11 +5,20 @@ namespace Patient.Tracker.API
 {
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
+        /// <summary>
+        /// Builds the web host.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
         public static IWebHost BuildWebHost(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
